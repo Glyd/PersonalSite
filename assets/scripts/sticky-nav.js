af192afe3153962a4542f33076d6ee0c8bdd4a36
Darius Lewis <darius.lewis@bbc.co.uk>
2017-02-27 14:52:57 +0000

    Hide show/hide button when javascript is disabled

--- assets/scripts/sticky-nav.js
+++ assets/scripts/sticky-nav.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
-  $('.show-button').removeClass("form-hide");
+  $('.show-button').removeClass("form-hide").addClass("form-show");
   $('#contact .container-content .content').removeClass("form-show").addClass("form-hide");
 });
+
 var  nav = $(".page-nav");
   navScrolled = "page-nav-scrolled";
   header = $('header').height();
