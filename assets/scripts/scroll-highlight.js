322ca070d2ab047bfc7a4cb7a6a21ebde77e0edd
Darius Lewis <darius.lewis@bbc.co.uk>
2017-03-02 10:27:49 +0000

    Reformat CSS. Added highlight scrolling to nav bar

---
+++ assets/scripts/scroll-highlight.js
@@ -0,0 +1,35 @@
+/* Not completely original - adaption to
+http://stackoverflow.com/questions/32395988/highlight-menu-item-when-scrolling-down-to-section */
+
+$(window).scroll(function() {
+    var position = $(this).scrollTop();
+
+    //highlight the item you are reading on the Navigation
+
+    $('section').each(function() {
+
+        var target = $(this).offset().top;
+        var id = $(this).attr('id');
+
+        //highlight earlier to account for minimised contact form
+
+        if (id != 'about')
+          target -= 250;
+
+        if (position >= target) {
+            $('.page-nav > ul > li > a').removeClass('active');
+            $('.page-nav > ul > li > a[href="#' + id + '"]').addClass('active');
+        }
+    });
+
+    //remove active highlighting when at the top of the page
+
+    $('.header-image').each(function() {
+
+        var target = $(this).offset().top;
+
+        if (position <= target) {
+            $('.page-nav > ul > li > a').removeClass('active');
+          }
+    });
+});
